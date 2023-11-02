d8cab89544d8f6dff454070dcc3340dbfec89665
Sofiane Abou Abderrahim <sofianeabouabderrahim@gmail.com>
2023-11-02 18:30:49 +0200

    compiled app.ts

--- app.js
+++ app.js
@@ -1,3 +1,42 @@
+// class User {
+//   name: string;
+//   private age: number;
+var __extends = (this && this.__extends) || (function () {
+    var extendStatics = function (d, b) {
+        extendStatics = Object.setPrototypeOf ||
+            ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
+            function (d, b) { for (var p in b) if (Object.prototype.hasOwnProperty.call(b, p)) d[p] = b[p]; };
+        return extendStatics(d, b);
+    };
+    return function (d, b) {
+        if (typeof b !== "function" && b !== null)
+            throw new TypeError("Class extends value " + String(b) + " is not a constructor or null");
+        extendStatics(d, b);
+        function __() { this.constructor = d; }
+        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+    };
+})();
+var User = /** @class */ (function () {
+    function User(name, age) {
+        this.name = name;
+        this.age = age;
+    }
+    User.prototype.print = function () {
+        console.log(this.name);
+    };
+    return User;
+}());
+var Admin = /** @class */ (function (_super) {
+    __extends(Admin, _super);
+    function Admin(name, age, permissions) {
+        var _this = _super.call(this, name, age) || this;
+        _this.permissions = permissions;
+        return _this;
+    }
+    return Admin;
+}(User));
+var user = new User('Max', 30);
+console.log(user.name);
 var num1Input = document.getElementById('num1');
 var num2Input = document.getElementById('num2');
 var buttonElement = document.querySelector('button');
