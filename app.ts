1dff726579c021f448ed0bc200e469531f06a0e9
Sofiane Abou Abderrahim <sofianeabouabderrahim@gmail.com>
2023-11-02 18:05:20 +0200

    shorten the class + added access modifiers in the constructor

--- app.ts
+++ app.ts
@@ -1,8 +1,15 @@
-class User {
-  name: string;
-  private age: number;
+// class User {
+//   name: string;
+//   private age: number;
 
-  constructor(name: string, age: number) {
+//   constructor(name: string, age: number) {
+//     this.name = name;
+//     this.age = age;
+//   }
+// }
+
+class User {
+  constructor(public name: string, private age: number) {
     this.name = name;
     this.age = age;
   }
