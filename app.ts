b83cebbc7dff591b58a3be5cf84c8db7c8a450be
Sofiane Abou Abderrahim <sofianeabouabderrahim@gmail.com>
2023-11-02 10:44:50 +0200

    type of undefined in a function

--- app.ts
+++ app.ts
@@ -2,7 +2,12 @@ function add(a: number, b: number) {
   return a + b;
 }
 
+function printResult(result): undefined {
+  console.log(result);
+  return undefined;
+}
+
 const result = add(5, 3);
 let isDone = false;
 
-console.log(result);
+printResult(result);
