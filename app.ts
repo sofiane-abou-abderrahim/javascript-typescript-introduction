9dcaa9274e0fd896e1144daa41c682a2658ae82e
Sofiane Abou Abderrahim <sofianeabouabderrahim@gmail.com>
2023-11-02 11:13:09 +0200

    type casting

--- app.ts
+++ app.ts
@@ -1,12 +1,23 @@
+const num1Input = document.getElementById('num1') as HTMLInputElement;
+const num2Input = <HTMLInputElement>document.getElementById('num2');
+const buttonElement = document.querySelector('button') as HTMLButtonElement;
+
 function add(a: number, b: number) {
   return a + b;
 }
 
-function printResult(result: any) {
+function printResult(result) {
   console.log(result);
 }
 
-const result = add(5, 3);
-let isDone = false;
+// const result = add(5, 3);
+// let isDone = false;
 
-printResult(result);
+// printResult(result);
+
+buttonElement.addEventListener('click', () => {
+  const num1 = +num1Input.value;
+  const num2 = +num2Input.value;
+  const result = add(num1, num2);
+  printResult(result);
+});
